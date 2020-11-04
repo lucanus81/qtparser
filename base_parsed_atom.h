073a2b10066110d66d4a480b99634f288f212d21
Luca Stoppa <luca.stoppa@codersinternational.com>
2020-11-04 16:14:11 +0100

    Fixed indentation #5

--- base_parsed_atom.h
+++ base_parsed_atom.h
@@ -6,18 +6,18 @@
 class base_parsed_atom
 {
 public:
-	virtual ~base_parsed_atom() {}
-	virtual void print_atom_info() const = 0;
+  virtual ~base_parsed_atom() {}
+  virtual void print_atom_info() const = 0;
 
 protected:
-	uint64_t size_{0};
-	std::string type_;
+  uint64_t size_{0};
+  std::string type_;
 
 protected:
-	base_parsed_atom(uint64_t size, std::string const& type)
-		: size_{size}, type_{type}
-	{
-	}
+  base_parsed_atom(uint64_t size, std::string const& type)
+    : size_{size}, type_{type}
+  {
+  }
 };
 
 #endif
--- buffer_reader.h
+++ buffer_reader.h
@@ -8,67 +8,64 @@
 #include <memory>
 
 struct buffer_reader {
-	enum class BYTES_ORDER { SWAP_BYTES, DO_NOT_SWAP_BYTES }; 
+  enum class BYTES_ORDER { SWAP_BYTES, DO_NOT_SWAP_BYTES }; 
 	
   /**
-	 * Constructor that opens a file in binary mode (read only)
-	 */
-	buffer_reader(std::string const& filename) {
-		file_.open(filename, std::ios::binary);
-		if (file_.is_open())
-		{
-			std::filesystem::path const path{filename};
-			total_size_ = std::filesystem::file_size(path);
-			bytes_left_ = total_size_;
-		}
-	}
-
-	buffer_reader(buffer_reader const&) = delete;
-	buffer_reader(buffer_reader&&) = delete;
-	buffer_reader& operator=(buffer_reader const&) = delete;
-	buffer_reader& operator=(buffer_reader&&) = delete;
+   * Constructor that opens a file in binary mode (read only)
+   */
+  buffer_reader(std::string const& filename) 
+  {
+    file_.open(filename, std::ios::binary);
+    if (file_.is_open())
+    {
+      std::filesystem::path const path{filename};
+      total_size_ = std::filesystem::file_size(path);
+      bytes_left_ = total_size_;
+    }
+  }
 
   /**
-	 * Method that return the total file size
-	 */
-	size_t size() const { return total_size_; }
-	
-	/**
-	 * This function is used to read a chunk of bytes and convert it into
-	 * any numeric value. Keep in mind, that as per Apple's specification,
-	 * all integers are stored in big-endian format.
-	 */
-	template <typename IntegerType, typename = std::enable_if_t<std::is_integral<IntegerType>::value>>
-	bool read(IntegerType& value, BYTES_ORDER operation=BYTES_ORDER::SWAP_BYTES) {
-		union {
-			char buffer[sizeof(IntegerType)];
-			IntegerType value;
-		} buffer_chunk{};
-		
-		file_.read(static_cast<char*>(buffer_chunk.buffer), sizeof(buffer_chunk.buffer));
-		if (operation == BYTES_ORDER::SWAP_BYTES)
-			swap_bytes(buffer_chunk.buffer, sizeof(buffer_chunk.buffer));
+   * Method that return the total file size
+   */
+  size_t size() const { return total_size_; }
 
-		value = buffer_chunk.value;
-		
-		size_t bytes_read = file_.gcount();
-		bytes_left_ -= bytes_read;
-		return sizeof(value) == bytes_read;
-	}
+  /**
+   * This function is used to read a chunk of bytes and convert it into
+   * any numeric value. Keep in mind, that as per Apple's specification,
+   * all integers are stored in big-endian format.
+   */
+  template <typename IntegerType, typename = std::enable_if_t<std::is_integral<IntegerType>::value>>
+  bool read(IntegerType& value, BYTES_ORDER operation=BYTES_ORDER::SWAP_BYTES)
+  {
+    union {
+      char buffer[sizeof(IntegerType)];
+      IntegerType value;
+    } buffer_chunk{};
 
-	bool read(std::unique_ptr<char[]>& buffer, size_t buffer_size)
-	{
-		file_.read(buffer.get(), buffer_size);
-		
-		size_t bytes_read = file_.gcount();
-		bytes_left_ -= bytes_read;
-		return buffer_size == file_.gcount();
-	}
+    file_.read(static_cast<char*>(buffer_chunk.buffer), sizeof(buffer_chunk.buffer));
+    if (operation == BYTES_ORDER::SWAP_BYTES)
+	    swap_bytes(buffer_chunk.buffer, sizeof(buffer_chunk.buffer));
 
-	bool has_more_bytes() const
-	{
-		return bytes_left_ > 0;
-	}
+    value = buffer_chunk.value;
+
+    size_t bytes_read = file_.gcount();
+    bytes_left_ -= bytes_read;
+    return sizeof(value) == bytes_read;
+  }
+
+  bool read(std::unique_ptr<char[]>& buffer, size_t buffer_size)
+  {
+    file_.read(buffer.get(), buffer_size);
+
+    size_t bytes_read = file_.gcount();
+    bytes_left_ -= bytes_read;
+    return buffer_size == file_.gcount();
+  }
+
+  bool has_more_bytes() const
+  {
+    return bytes_left_ > 0;
+  }
 
   size_t bytes_left() const
   {
@@ -76,16 +73,16 @@ struct buffer_reader {
   }
 
 private:
-	void swap_bytes(char* buffer, size_t size)
-	{
-		for (size_t i=0; i<size/2; ++i)
-			std::swap(buffer[i], buffer[size-1-i]);
-	}
+  void swap_bytes(char* buffer, size_t size)
+  {
+    for (size_t i=0; i<size/2; ++i)
+      std::swap(buffer[i], buffer[size-1-i]);
+  }
 
 private:
-	std::ifstream file_;
-	size_t total_size_{0};;
-	size_t bytes_left_{0};
+  std::ifstream file_;
+  size_t total_size_{0};;
+  size_t bytes_left_{0};
 };
 
 #endif
